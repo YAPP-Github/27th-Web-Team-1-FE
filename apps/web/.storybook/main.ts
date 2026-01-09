70eeda3651423f2f55a9e297d9f0486d8fedb1d1
H0ngJu <hongju4385@gmail.com>
2026-01-09 22:39:19 +0900

    chore: storybook 설정 변경
    - 사용하지 않는 애드온 우선적으로 제거
    - vite preview 활성화
    - storybook 전용 queryclient 추가

--- apps/web/.storybook/main.ts
+++ apps/web/.storybook/main.ts
@@ -1,31 +1,11 @@
-import type { StorybookConfig } from '@storybook/react-vite';
-import tsconfigPaths from "vite-tsconfig-paths";
+// .storybook/main.ts
+import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(ts|tsx)'],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-interactions'],
+  stories: ['../src/**/*.stories.@(ts|tsx|mdx)'],
   framework: {
     name: '@storybook/nextjs',
     options: {},
-  },
-  docs: {
-    autodocs: 'tag',
-  },
-  core: {
-    builder: "@storybook/builder-vite",
-  },
-  async viteFinal(config) {
-    config.plugins = config.plugins ?? [];
-    config.plugins.push(tsconfigPaths());
-    config.esbuild = {
-      ...(config.esbuild ?? {}),
-      jsx: 'automatic',
-    };
-    config.define = {
-      ...config.define,
-      "process.env": {},
-    };
-    return config;
   },
 };
 
