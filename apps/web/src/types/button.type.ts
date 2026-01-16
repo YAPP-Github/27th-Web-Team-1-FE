5fa349bed0981cec2299b960de4737f8dac57d72
H0ngJu <hongju4385@gmail.com>
2026-01-16 23:50:45 +0900

    feat: Chip 컴포넌트 추가

--- apps/web/src/types/button.type.ts
+++ apps/web/src/types/button.type.ts
@@ -1,2 +1,4 @@
 export type ButtonSize = 'small' | 'medium' | 'large';
 export type ButtonVariant = 'highlight' | 'primary' | 'secondary';
+export type ChipSize = 'small' | 'medium';
+export type ChipVariant = 'black' | 'white';
