ae17ca17cefd1627c336b3712df8a3c99a60ed0a
H0ngJu <hongju4385@gmail.com>
2026-01-17 15:37:23 +0900

    test: Floating Button 스토리북 추가

---
+++ apps/web/src/stories/FloatingButton.stories.tsx
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import MapPinSvg from "@/assets/images/mapPin.svg"
+import FloatingButton from '@/components/buttons/floatingButton/FloatingButton';
+
+const meta: Meta<typeof FloatingButton> = {
+  title: 'Components/FloatingButton',
+  component: FloatingButton,
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof FloatingButton>;
+
+export const Default: Story = {
+  args: {
+    text: "위치로 이동",
+    onClick: () => {},
+  },
+};
+
+export const WithIcon: Story = {
+  args: {
+    text: "위치로 이동",
+    icon: <MapPinSvg />,
+    onClick: () => {},
+  },
+};
