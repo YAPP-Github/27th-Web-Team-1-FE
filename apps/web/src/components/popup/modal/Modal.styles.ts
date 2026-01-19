66f906b38b3a8e33efb49880eb21fabd1673b192
H0ngJu <hongju4385@gmail.com>
2026-01-20 00:41:00 +0900

    feat: 모달 컴포넌트 및 스타일 추가

---
+++ apps/web/src/components/popup/modal/Modal.styles.ts
@@ -0,0 +1,22 @@
+import styled from '@emotion/styled';
+
+export const Content = styled.div`
+  /* TODO: 추후 반응형 디자인에 따른 width, height 설정 필요 */
+  min-width: 280px;
+  max-width: calc(100% - 32px);
+  max-height: calc(100% - 32px);
+  padding: 24px;
+  background: ${({ theme }) => theme.colors.gray[1000]};
+  border: 1px solid ${({ theme }) => theme.colors.blueWhite.border10};
+  border-radius: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 24px;
+`;
+
+export const Footer = styled.footer`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
---
+++ apps/web/src/components/popup/modal/Modal.tsx
@@ -0,0 +1,44 @@
+import PopupRoot from '../PopupRoot';
+import PopupLayer from '../PopupLayer';
+import Backdrop from '../BackDrop';
+import * as S from './Modal.styles';
+
+interface ModalProps {
+  /** 모달 열림 상태 */
+  isOpen: boolean;
+  /** 모달 닫는 함수 */
+  onClose: () => void;
+  children: React.ReactNode;
+}
+
+interface ModalContentProps {
+  children: React.ReactNode;
+}
+
+const Modal = ({ isOpen, onClose, children }: ModalProps) => {
+  if (!isOpen) return null;
+
+  return (
+    <PopupRoot>
+      <Backdrop onClick={onClose} />
+      <PopupLayer>{children}</PopupLayer>
+    </PopupRoot>
+  );
+};
+
+const ModalContent = ({ children }: ModalContentProps) => {
+  return (
+    <S.Content role="dialog" aria-modal="true">
+      {children}
+    </S.Content>
+  );
+};
+
+const ModalFooter = ({ children }: { children: React.ReactNode }) => {
+  return <S.Footer>{children}</S.Footer>;
+};
+
+Modal.Content = ModalContent;
+Modal.Footer = ModalFooter;
+
+export default Modal;
