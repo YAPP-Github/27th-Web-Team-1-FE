e9055dd36ac9c843511c452b43c380ae6e98d093
H0ngJu <hongju4385@gmail.com>
2026-01-20 23:28:39 +0900

    refactor: body가 아닌 modal-root에 팝업이 나오도록 수정

--- apps/web/src/components/popup/PopupRoot.tsx
+++ apps/web/src/components/popup/PopupRoot.tsx
@@ -1,12 +1,19 @@
+'use client';
+
 import { createPortal } from 'react-dom';
+import { useEffect, useState } from 'react';
 import * as S from './Popup.styles';
 
-interface PopupRootProps {
-  children: React.ReactNode;
-}
+const PopupRoot = ({ children }: { children: React.ReactNode }) => {
+  const [modalRoot, setModalRoot] = useState<HTMLElement | null>(null);
 
-const PopupRoot = ({ children }: PopupRootProps) => {
-  return createPortal(<S.Root>{children}</S.Root>, document.body);
+  useEffect(() => {
+    setModalRoot(document.getElementById('modal-root'));
+  }, []);
+
+  if (!modalRoot) return null;
+
+  return createPortal(<S.Root>{children}</S.Root>, modalRoot);
 };
 
 export default PopupRoot;
