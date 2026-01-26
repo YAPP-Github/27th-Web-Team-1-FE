ccd935fcd95ffd07c4a0afcefa887fa9ca411efd
salmonco <xkqjsslsek80@naver.com>
2026-01-27 01:29:05 +0900

    feat: update modal style

--- apps/web/src/components/popup/modal/Modal.styles.ts
+++ apps/web/src/components/popup/modal/Modal.styles.ts
@@ -2,9 +2,9 @@ import styled from '@emotion/styled';
 
 export const Content = styled.div`
   /* TODO: 추후 반응형 디자인에 따른 width, height 설정 필요 */
-  min-width: 280px;
-  max-width: calc(100% - 32px);
-  max-height: calc(100% - 32px);
+  width: calc(100% - 40px);
+  max-width: calc(100% - 40px);
+  max-height: calc(100% - 40px);
   padding: 24px;
   background: ${({ theme }) => theme.colors.gray[1000]};
   border: 1px solid ${({ theme }) => theme.colors.blueWhite.border10};
