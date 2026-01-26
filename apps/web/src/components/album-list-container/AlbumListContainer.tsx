1edd790d1233dad71987ce3512f276715ffc372a
salmonco <xkqjsslsek80@naver.com>
2026-01-27 01:11:36 +0900

    feat: add component

---
+++ apps/web/src/components/album-list-container/AlbumListContainer.tsx
@@ -0,0 +1,41 @@
+import AlbumRow from '../album-row/AlbumRow';
+import * as S from './AlbumListContainer.styles';
+
+interface Album {
+  id: string;
+  title: string;
+  thumbnail: string;
+  photoCount: number;
+}
+
+interface AlbumListContainerProps {
+  /** 앨범 목록 */
+  albums: Album[];
+  /** 선택된 앨범 ID */
+  selectedAlbumId: string | null;
+  /** 앨범 선택 핸들러 */
+  onSelectAlbum: (albumId: string) => void;
+}
+
+const AlbumListContainer = ({
+  albums,
+  selectedAlbumId,
+  onSelectAlbum,
+}: AlbumListContainerProps) => {
+  return (
+    <S.Container>
+      {albums.map((album) => (
+        <AlbumRow
+          key={album.id}
+          title={album.title}
+          thumbnail={album.thumbnail}
+          photoCount={album.photoCount}
+          onClick={() => onSelectAlbum(album.id)}
+          isSelected={selectedAlbumId === album.id}
+        />
+      ))}
+    </S.Container>
+  );
+};
+
+export default AlbumListContainer;
---
+++ apps/web/src/components/album-row/AlbumRow.tsx
@@ -0,0 +1,37 @@
+import * as S from './AlbumRow.styles';
+
+interface AlbumRowProps {
+  /** 앨범 제목 */
+  title: string;
+  /** 앨범 썸네일 이미지 URL */
+  thumbnail: string;
+  /** 사진 개수 */
+  photoCount: number;
+  /** 클릭 핸들러 */
+  onClick: () => void;
+  /** 선택 여부 */
+  isSelected: boolean;
+}
+
+const AlbumRow = ({
+  title,
+  thumbnail,
+  photoCount,
+  onClick,
+  isSelected,
+}: AlbumRowProps) => {
+  return (
+    <S.Container onClick={onClick}>
+      <S.InfoContainer>
+        <S.Thumbnail src={thumbnail} alt={title} />
+        <S.TextSection>
+          <S.Title>{title}</S.Title>
+          <S.PhotoCount>{photoCount}</S.PhotoCount>
+        </S.TextSection>
+      </S.InfoContainer>
+      <S.RadioButton isSelected={isSelected} />
+    </S.Container>
+  );
+};
+
+export default AlbumRow;
