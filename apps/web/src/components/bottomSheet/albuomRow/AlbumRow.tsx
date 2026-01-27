7c73cddd5e9bf9be0f3f6beffb5a99e4f3e1a413
H0ngJu <hongju4385@gmail.com>
2026-01-27 16:31:35 +0900

    feat: 바텀시트 small일 때 보여줄 AlbumRow 컴포넌트 추가

---
+++ apps/web/src/components/bottomSheet/albuomRow/AlbumRow.tsx
@@ -0,0 +1,57 @@
+import { Album } from "@/types/album.type";
+import * as S from "./AlbumRow.styles"
+import AlbumContainer from "@/components/album-container/AlbumContainer";
+import { useRef } from "react";
+
+interface AlbumRowProps {
+    albums: Album[];
+}
+
+const AlbumRow = ({ albums }: AlbumRowProps) => {
+    const containerRef = useRef<HTMLDivElement>(null);
+    const isDragging = useRef(false);
+    const startX = useRef(0);
+    const scrollLeft = useRef(0);
+  
+    const onMouseDown = (e: React.MouseEvent) => {
+      isDragging.current = true;
+      startX.current = e.pageX;
+      scrollLeft.current = containerRef.current!.scrollLeft;
+    };
+  
+    const onMouseMove = (e: React.MouseEvent) => {
+      if (!isDragging.current) return;
+      e.preventDefault();
+  
+      const x = e.pageX;
+      const walk = x - startX.current;
+      containerRef.current!.scrollLeft = scrollLeft.current - walk;
+    };
+  
+    const onMouseUp = () => {
+      isDragging.current = false;
+    };
+  
+    return (
+      <S.Wrapper
+        ref={containerRef}
+        onMouseDown={onMouseDown}
+        onMouseMove={onMouseMove}
+        onMouseUp={onMouseUp}
+        onMouseLeave={onMouseUp}
+      >
+        {albums.map((album, index) => (
+          <AlbumContainer
+            key={index}
+            title={album.title}
+            type="small"
+            photoList={album.photoList}
+            photoCount={album.photoList.length}
+          />
+        ))}
+      </S.Wrapper>
+    );
+  };
+  
+
+export default AlbumRow;
