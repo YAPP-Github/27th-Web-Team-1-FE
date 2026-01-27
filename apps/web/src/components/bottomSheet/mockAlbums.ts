2c210cb42ca11504d82237ff7c9d5bc405892e79
H0ngJu <hongju4385@gmail.com>
2026-01-27 16:47:34 +0900

    chore: Map용 임시 MockData 추가

---
+++ apps/web/src/components/bottomSheet/mockAlbums.ts
@@ -0,0 +1,128 @@
+import { Album } from '@/types/album.type';
+
+export const mockAlbums: Album[] = [
+  {
+    title: '여행',
+    photoList: [
+      {
+        photoId: '1',
+        src: 'https://picsum.photos/200/200',
+      },
+      {
+        photoId: '2',
+        src: 'https://picsum.photos/200/200',
+      },
+      {
+        photoId: '3',
+        src: 'https://picsum.photos/200/200',
+      },
+    ],
+  },
+  {
+    title: '일상',
+    photoList: [
+      {
+        photoId: '4',
+        src: 'https://picsum.photos/200/200',
+      },
+      {
+        photoId: '5',
+        src: 'https://picsum.photos/200/200',
+      },
+      {
+        photoId: '5',
+        src: 'https://picsum.photos/200/200',
+      },
+      {
+        photoId: '5',
+        src: 'https://picsum.photos/200/200',
+      },
+      {
+        photoId: '5',
+        src: 'https://picsum.photos/200/200',
+      },
+      {
+        photoId: '5',
+        src: 'https://picsum.photos/200/200',
+      },
+    ],
+  },
+  {
+    title: '일상',
+    photoList: [
+      {
+        photoId: '4',
+        src: 'https://picsum.photos/200/200',
+      },
+      {
+        photoId: '5',
+        src: 'https://picsum.photos/200/200',
+      },
+    ],
+  },
+  {
+    title: '일상',
+    photoList: [
+      {
+        photoId: '4',
+        src: 'https://picsum.photos/200/200',
+      },
+      {
+        photoId: '5',
+        src: 'https://picsum.photos/200/200',
+      },
+    ],
+  },
+  {
+    title: '일상',
+    photoList: [
+      {
+        photoId: '4',
+        src: 'https://picsum.photos/200/200',
+      },
+      {
+        photoId: '5',
+        src: 'https://picsum.photos/200/200',
+      },
+    ],
+  },
+  {
+    title: '일상',
+    photoList: [
+      {
+        photoId: '4',
+        src: 'https://picsum.photos/200/200',
+      },
+      {
+        photoId: '5',
+        src: 'https://picsum.photos/200/200',
+      },
+    ],
+  },
+  {
+    title: '일상',
+    photoList: [
+      {
+        photoId: '4',
+        src: 'https://picsum.photos/200/200',
+      },
+      {
+        photoId: '5',
+        src: 'https://picsum.photos/200/200',
+      },
+    ],
+  },
+  {
+    title: '일상',
+    photoList: [
+      {
+        photoId: '4',
+        src: 'https://picsum.photos/200/200',
+      },
+      {
+        photoId: '5',
+        src: 'https://picsum.photos/200/200',
+      },
+    ],
+  },
+];
