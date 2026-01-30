d685289ab019e8c65a1c36ab1f08624213c1d98b
salmonco <xkqjsslsek80@naver.com>
2026-01-31 00:45:42 +0900

    feat: handle zero

--- apps/web/src/auth/cookies.ts
+++ apps/web/src/auth/cookies.ts
@@ -50,5 +50,5 @@ export const clearUserIdCookie = () => {
 
 export const getAuthorizationHeader = () => {
   const userId = getUserIdFromCookie();
-  return userId ? String(userId) : undefined;
+  return userId !== null ? String(userId) : undefined;
 };
