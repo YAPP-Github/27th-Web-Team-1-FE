e140e0e8c2010e4c1378266c1ff67b9dbeb0d3b4
H0ngJu <hongju4385@gmail.com>
2026-01-31 04:26:50 +0900

    feat: 지도뷰 미리보기 추가

---
+++ apps/web/src/components/map/MapPreview.styles.ts
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  z-index: 1000;
+`;
+
+export const MapContainer = styled.div`
+  flex: 1;
+  position: relative;
+  width: 100%;
+`;
