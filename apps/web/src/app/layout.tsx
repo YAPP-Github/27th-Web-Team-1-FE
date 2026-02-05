3e42aa86d4ace1260115ccd13665290bfd829027
H0ngJu <hongju4385@gmail.com>
2026-02-05 23:29:25 +0900

    refactor: 로컬에서 수집되지 않도록 분리

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -36,6 +36,8 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const beusableId = process.env.NEXT_PUBLIC_BEUSABLE_ID;
+
   return (
     <html lang="ko">
       <body className={pretendard.variable}>
@@ -46,24 +48,26 @@ export default function RootLayout({
         </AppProviders>
         <div id="modal-root" />
         <div id="toast-root" />
-        <Script
-          id="beusable-rum"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w, d, a){
-                w.beusablerumclient = {
-                    load : function(src){
-                        var b = d.createElement("script");
-                        b.src = src; b.async=true; b.type = "text/javascript";
-                        d.getElementsByTagName("head")[0].appendChild(b);
-                    }
-                };
-                w.beusablerumclient.load(a + "?url=" + encodeURIComponent(d.URL));
-              })(window, document, "//rum.beusable.net/load/b260205e223421u488");
-            `,
-          }}
-        />
+        {beusableId && (
+          <Script
+            id="beusable-rum"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(w, d, a){
+                  w.beusablerumclient = {
+                      load : function(src){
+                          var b = d.createElement("script");
+                          b.src = src; b.async=true; b.type = "text/javascript";
+                          d.getElementsByTagName("head")[0].appendChild(b);
+                      }
+                  };
+                  w.beusablerumclient.load(a + "?url=" + encodeURIComponent(d.URL));
+                })(window, document, "//rum.beusable.net/load/${beusableId}");
+              `,
+            }}
+          />
+        )}
       </body>
     </html>
   );
