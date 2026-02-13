490691d5cfea2d886f00a6391b0b213be3b3abbe
H0ngJu <hongju4385@gmail.com>
2026-02-13 19:55:03 +0900

    refactor: 줌 레벨 수정

--- apps/web/src/constants/map.ts
+++ apps/web/src/constants/map.ts
@@ -3,7 +3,7 @@
  */
 export const MAP_CLUSTERING_CONFIG = {
   /** 클라이언트 클러스터링 시작 줌 레벨 (이 레벨 이상에서 클라이언트 클러스터링 사용) */
-  CLIENT_CLUSTERING_MIN_ZOOM: 17,
+  CLIENT_CLUSTERING_MIN_ZOOM: 15,
   /** Supercluster 최대 줌 레벨 */
   SUPERCLUSTER_MAX_ZOOM: 20,
   /** Supercluster 클러스터 반지름 (픽셀) */
