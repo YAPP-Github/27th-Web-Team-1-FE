74a4f6715d81a1f8d3893444f2095be8c96402eb
salmonco <xkqjsslsek80@naver.com>
2026-02-23 02:25:17 +0900

    feat: design footer

--- apps/web/src/app/mypage/_components/Footer/Footer.tsx
+++ apps/web/src/app/mypage/_components/Footer/Footer.tsx
@@ -3,9 +3,13 @@ import styles from './Footer.module.css';
 export default function Footer() {
   return (
     <footer className={styles.wrapper}>
-      <button className={styles.footerButton}>로그아웃</button>
+      <button type="button" className={styles.footerButton}>
+        로그아웃
+      </button>
       <span className={styles.divider} />
-      <button className={styles.footerButton}>탈퇴하기</button>
+      <button type="button" className={styles.footerButton}>
+        회원 탈퇴
+      </button>
     </footer>
   );
 }
