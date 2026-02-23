d69dfbb14e855803e7ee21e0d5da0486d3d100b1
salmonco <xkqjsslsek80@naver.com>
2026-02-24 05:40:29 +0900

    feat: update style

--- apps/web/src/app/mypage/_components/CoupleInfoContainer/_components/ProfileContainer/_clientBoundary/NicknameClient/NicknameClient.styles.ts
+++ apps/web/src/app/mypage/_components/CoupleInfoContainer/_components/ProfileContainer/_clientBoundary/NicknameClient/NicknameClient.styles.ts
@@ -1,9 +1,10 @@
 import styled from '@emotion/styled';
 
 export const Wrapper = styled.div`
+  position: relative;
   display: flex;
   align-items: center;
-  gap: 4px;
+  justify-content: center;
   cursor: pointer;
   max-width: 100%;
 `;
@@ -17,6 +18,8 @@ export const Text = styled.span`
 `;
 
 export const EditIcon = styled.span`
+  position: absolute;
+  right: -16px; /* gap(4) + icon(12) */
   display: flex;
   align-items: center;
   color: ${({ theme }) => theme.colors.gray[400]};
