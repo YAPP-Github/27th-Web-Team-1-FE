f84da4040795fdd14f5ef3a84c9910495d9791d9
salmonco <xkqjsslsek80@naver.com>
2026-02-25 00:40:22 +0900

    chore: remove log

--- apps/web/src/instrumentation.ts
+++ apps/web/src/instrumentation.ts
@@ -1,12 +1,4 @@
 export async function register() {
-  console.log(
-    '[instrumentation] register called',
-    'ENABLE_MOCK:',
-    process.env.NEXT_PUBLIC_ENABLE_MOCK,
-    'RUNTIME:',
-    process.env.NEXT_RUNTIME,
-  );
-
   if (
     process.env.NEXT_PUBLIC_ENABLE_MOCK === 'true' &&
     process.env.NEXT_RUNTIME === 'nodejs'
