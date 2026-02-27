acd0e20fa9abebeeea0f195120d315069b99de66
H0ngJu <hongju4385@gmail.com>
2026-02-27 16:48:09 +0900

    refactor: ui 백그라운드 이상 해결

--- apps/web/src/app/onboarding/connect/page.styles.ts
+++ apps/web/src/app/onboarding/connect/page.styles.ts
@@ -25,6 +25,7 @@ export const Wrapper = styled.div`
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
+  min-height: 100vh;
   min-height: 100dvh;
   padding: 0;
   background-color: ${({ theme }) => theme.colors.gray[1000]};
--- apps/web/src/app/onboarding/verify/page.styles.ts
+++ apps/web/src/app/onboarding/verify/page.styles.ts
@@ -34,6 +34,7 @@ export const Container = styled.div`
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
+  min-height: 100vh;
   min-height: 100dvh;
   padding: 0;
   background-color: ${({ theme }) => theme.colors.gray[1000]};
